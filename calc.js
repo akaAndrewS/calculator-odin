7fe6c7c4c00fbbb72a84b3d57046b9ad6b14ab6b
akaAndrewS <132374843+akaAndrewS@users.noreply.github.com>
2023-06-02 16:46:16 -0400

    Add functionality of buttons to calc.js

--- calc.js
+++ calc.js
@@ -1,4 +1,93 @@
-let userNum1, userNum2, operator;
+let userNum1 = ''; 
+let userNum2 = '';
+let operator = '';
+let result = '';
+
+let display = document.querySelector('.display');
+let displayValue = display.textContent;
+let buttons = document.querySelectorAll('button');
+buttons.forEach( (button) => {
+    button.addEventListener('click', () => buttonPress(button));
+});
+
+// buttonPress() function that determines which button was pressed and reacts accordingly
+function buttonPress(button) {
+    switch(button.textContent) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+            if(!(result === '')) {
+                clearDisplay();
+                result = '';
+            }
+            displayValue += button.textContent;
+            displayOutput();
+            if (operator === '') {
+                userNum1 += button.textContent;
+                console.log(userNum1);
+
+            }
+            else {
+                userNum2 += button.textContent;
+                console.log(userNum2);
+            }
+            console.log("Getting there");
+            break;
+        case '+':
+        case '-':
+            //special case for negative numbers need
+        case '*':
+        case '/':
+            //IF there is not an operator and a number is before the operator
+            if (operator === '' && !(userNum1 === '')) {
+                console.log('GEtting closer');
+                //store the number before the operator in userNum1
+                displayValue += ` ${button.textContent} `;
+                operator = button.textContent;
+                displayOutput();
+            }
+            break;
+        case '=':
+            //needs to store the number after the operator in userNum2
+            if(!(userNum2 === '')) {
+                console.log('so close!');
+                userNum1 = +userNum1;
+                userNum2 = +userNum2;
+                result = operate(userNum1, userNum2, operator);
+                clearDisplay();
+                displayValue = result;
+                displayOutput();
+            }
+            //needs to ensure that a nuber is after the operator
+            break;
+        case 'Clear':
+            clearDisplay();
+            break;
+
+        
+    }
+}
+
+// clearDisplay() function that resets userNum1, userNum2, operator and displayValue
+function clearDisplay() {
+    userNum1 = '';
+    userNum2 = '';
+    operator = '';
+    displayValue = '';
+    displayOutput();
+}
+
+// displayInput() funciton that displays the current user input
+function displayOutput() {
+    display.textContent = displayValue;
+}
 
 // add() function takes two numbers and returns there sum
 function add (num1, num2) {
@@ -36,6 +125,7 @@ function operate (num1, num2, operator1) {
     }
 }
 
+/*
 // TEST CALLS //FIXME --------------------
 // test add(), subtract(), multiply(), divide() functions
 for (let i = 0; i < 100; i++) {
@@ -71,4 +161,4 @@ for (let i = 0; i < 100; i++) {
         }
     }
 }
-console.log("Done");
+console.log("Done");*/
