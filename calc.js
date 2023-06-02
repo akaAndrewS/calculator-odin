b3df0c26568da9237de0e9457c777ab47a5472d8
akaAndrewS <132374843+akaAndrewS@users.noreply.github.com>
2023-06-02 14:29:27 -0400

    Add operate function to calc.js and test function

--- calc.js
+++ calc.js
@@ -1,3 +1,5 @@
+let userNum1, userNum2, operator;
+
 // add() function takes two numbers and returns there sum
 function add (num1, num2) {
     return num1 + num2;
@@ -12,13 +14,30 @@ function multiply (num1, num2) {
 }
 // divide() function takes two numbers and returns their quotient
 function divide (num1, num2) {
+    //IF trying to divide by zero
     if (num2 ===0) {
+        //return error
         return "ERR";
     }
     return num1 / num2;
 }
 
+// operate() function that takes two numbers and an operator and calls appropriate function
+function operate (num1, num2, operator1) {
+    switch(operator1) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
 // TEST CALLS //FIXME --------------------
+// test add(), subtract(), multiply(), divide() functions
 for (let i = 0; i < 100; i++) {
     for (let j = 0; j < 100; j++) {
         if (!(add(i, j) === i + j)) {
@@ -30,9 +49,26 @@ for (let i = 0; i < 100; i++) {
         if (!(multiply(i, j) === i * j)) {
             console.log(`multiply of ${i} and ${j} is false`);
         }
-        if (!(divide(i, j) === i / j)) {
+        if (!(divide(i, j) === i / j) && !(divide(i, j) === "ERR")) {
             console.log(`divide of ${i} and ${j} is false`);
         }
     }
-    console.log("Done");
 }
+// test operate function calling add(), subtract(), multiply(), divide() functions
+for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+        if (!(operate(i, j, '+') === i + j)) {
+            console.log(`sum of ${i} and ${j} is false`);
+        }
+        if (!(operate(i, j, '-') === i - j)) {
+            console.log(`subtract of ${i} and ${j} is false`);
+        }
+        if (!(operate(i, j, '*') === i * j)) {
+            console.log(`multiply of ${i} and ${j} is false`);
+        }
+        if (!(operate(i, j, '/') === i / j) && !(operate(i, j, '/') === "ERR")) {
+            console.log(`divide of ${i} and ${j} is false`);
+        }
+    }
+}
+console.log("Done");
