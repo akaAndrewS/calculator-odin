f5776a05ce75dfaa1ff97cd1314a6d017f8d0459
akaAndrewS <132374843+akaAndrewS@users.noreply.github.com>
2023-06-04 09:41:51 -0400

    Add ability to chain operations together to calc.js

--- calc.js
+++ calc.js
@@ -23,55 +23,88 @@ function buttonPress(button) {
         case '8':
         case '9':
         case '0':
+            //IF there is a result
             if(!(result === '')) {
                 clearDisplay();
                 result = '';
             }
-            displayValue += button.textContent;
-            displayOutput();
+            //IF there is not currently an operator stored
             if (operator === '') {
+                //add button's value to 'displayValue' and display
+                displayValue += button.textContent;
+                displayOutput();
+                //save the number to userNum1
                 userNum1 += button.textContent;
-                console.log(userNum1);
+                console.log(`userNum1 is ${userNum1}`);  //FIXME ----------------------------
 
             }
-            else {
+            //IF there is an operator stored but no userNum2
+            else if (userNum2 ==='') {
+                //remove the display value
+                displayValue = '';
+                //add button's value to 'displayValue' and display
+                displayValue += button.textContent;
+                displayOutput();
+                //save the number to userNum2
                 userNum2 += button.textContent;
-                console.log(userNum2);
+                console.log(`userNum2 is ${userNum2}`);  //FIXME ------------------------------
             }
-            console.log("Getting there");
+            else {
+                //add button's value to 'displayValue' and display
+                displayValue += button.textContent;
+                displayOutput();
+                //save the number to userNum2
+                userNum2 += button.textContent;
+                console.log(`userNum2 is ${userNum2}`);  //FIXME ------------------------------
+            }
             break;
         case '+':
         case '-':
             //special case for negative numbers need
         case '*':
         case '/':
-            //IF there is not an operator and a number is before the operator
-            if (operator === '' && !(userNum1 === '')) {
-                console.log('GEtting closer');
-                //store the number before the operator in userNum1
-                displayValue += ` ${button.textContent} `;
-                operator = button.textContent;
-                displayOutput();
+            //IF there is a number stored in userNum1
+            if (!(userNum1 === '')) {
+                //IF there is also a number stored in userNum2
+                if (!(userNum2 === '')) {
+                    //get the result and display the result
+                    result = operate(+userNum1, +userNum2, operator);
+                    displayValue = Math.round((result + Number.EPSILON) * 1000000000) / 1000000000;
+                    displayOutput();
+                    //set userNum1 to the result and clear userNum2 and result
+                    userNum1 = `${result}`;
+                    userNum2 = '';
+                    result = '';
+                    //save the new operator
+                    operator = button.textContent;
+                    console.log(`userNum1 is ${userNum1}, operator is ${operator}`);  //FIXME ------------------------------
+                }
+                //ELSE just save the operator
+                else {
+                    operator = button.textContent;
+                    console.log(operator); //FIXME ==================------------
+                }
             }
+            
             break;
         case '=':
             //needs to store the number after the operator in userNum2
             if(!(userNum2 === '')) {
-                console.log('so close!');
-                userNum1 = +userNum1;
-                userNum2 = +userNum2;
-                result = operate(userNum1, userNum2, operator);
-                clearDisplay();
-                displayValue = result;
+                //get and display the result
+                result = operate(+userNum1, +userNum2, operator);
+                displayValue = Math.round((result + Number.EPSILON) * 1000000000) / 1000000000;
                 displayOutput();
+                //set userNum1 to the result and clear userNum2 and result
+                userNum1 = `${result}`;
+                userNum2 = '';
+                result = '';
+                console.log(`userNum1 is ${userNum1}`);  //FIXME ------------------------------
             }
             //needs to ensure that a nuber is after the operator
             break;
         case 'Clear':
             clearDisplay();
             break;
-
-        
     }
 }
 
